b609347f42a4613a5e7050eadac2894fdfd47962
Darji Vrund <vrunddarji2005@gmail.com>
2026-05-07 17:06:54 +0530

    updated

--- frontend/src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://indiahouse.onrender.com/api',
-  socketUrl: 'https://indiahouse.onrender.com',
-  googleClientId: '1021134306283-igdic6fv5o89jo3io59thuhgiiipntg4.apps.googleusercontent.com'
-};
